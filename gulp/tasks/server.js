151ef1c74a80573f8b16456b6756e40f5916607f
fedyaka <68501592+fedyaka@users.noreply.github.com>
2022-12-13 11:25:18 +0300

    Добавление зависимости connect-history-api-fallback
    Она нужна для сохранения истории перехода по сайту, но в html файле теперь нужно в ссылках к файлам указывать в начале "/". Так же без него не будет работать react router и при перезагрузки странички или повторного перехода будешь получать cannot GET /url.

--- gulp/tasks/server.js
+++ gulp/tasks/server.js
@@ -1,9 +1,21 @@
+import historyApiFallback from 'connect-history-api-fallback';
+
 export const server = () => {
     app.plugins.browserSync.init({
         server: {
-            baseDir: app.path.build.html
+            baseDir: app.path.build.html,
+            middleware: [historyApiFallback()] //нужен для адекватной работы router react, так же нужно использовать "/" в начале пути к файлам в html
         },
         notify: true,
         port: 3000
+        // //нужно для выключения Content Security Policy, если есть проблемы, то раскоментить
+        // rewriteRules: [
+        //     {
+        //         match: /Content-Security-Policy/,
+        //         fn: function (match) {
+        //             return "DISABLED-Content-Security-Policy";
+        //         }
+        //     }
+        // ]
     })
 };
